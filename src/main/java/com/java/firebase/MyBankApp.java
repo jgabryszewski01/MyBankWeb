c8ce6ced2f10cb5368583ed57923a5a3df9e7d21
jgabryszewski01 <jgabryszewski01@gmail.com>
2023-06-08 16:58:30 +0200

    feat: Add Firebase integration to the application

---
+++ src/main/java/com/java/firebase/MyBankApp.java
@@ -0,0 +1,38 @@
+package com.java.firebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Objects;
+
+@SpringBootApplication
+public class MyBankApp {
+    public static void main(String []args){
+        ClassLoader classLoader = MyBankApp.class.getClassLoader();
+
+        File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+        try {
+            FileInputStream refreshToken = new FileInputStream(file.getAbsolutePath());
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                    .setDatabaseUrl("https://mybank-2bfb7-default-rtdb.firebaseio.com")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        SpringApplication.run(MyBankApp.class, args);
+
+
+    }
+}
